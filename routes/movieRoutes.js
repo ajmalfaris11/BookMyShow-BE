5b3f31c5565489cbd9e181a5b475844ef1d343a2
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-16 02:21:09 +0530

    correct routing path for Get Movie by ID and improve consistency in route handling

---
+++ routes/movieRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+
+// 1 - Get all movies
+
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+// 2 - Get Movie by ID
+
+router.get('movies/:movieId', (req, res) => {
+  res.send('hello World!')
+})
+
+// 3 - Add Movie
+
+router.post('/', (req, res) => {
+  res.send('hello world!')
+})
+
+// 4 - Update Movie
+
+router.patch('/:movieId', (req, res) => {
+  res.send('hello world')
+})
+
+// 5 - Delete Movie
+
+router.delete('/:movieId', (req, res) => {
+  res.send('hello world')
+})
+
+module.exports = router
