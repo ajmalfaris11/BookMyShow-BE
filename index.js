644e002e75d24e28eafbe365cfe9c7e91a7b2199
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-17 03:56:39 +0530

    Added express.json() middleware to automatically parse incoming JSON data in request bodies. This simplifies handling JSON data in API requests.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require ('express')
 const movieRoutes = require('./routes/movieRoutes')
 const genreRoutes = require('./routes/genreRoutes')
-
 const app = express()
 const port = 3000
+
+// Using express.json() middleware to automatically parse incoming JSON payloads.
+// This allows the API to process the request body as a JavaScript object, making
+// it accessible via req.body in route handlers.
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
