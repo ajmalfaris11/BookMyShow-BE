01e91fa6e3df55dbe5c81d482995de29fc4a8bcb
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-17 02:05:53 +0530

    feat: implement genre routes for CRUD operations
    - Add route to fetch all genres (`GET /`)
    - Add route to fetch a genre by ID (`GET /genres/:genreId`)
    - Add route to create a new genre (`POST /`)
    - Add route to update a genre (`PATCH /:genreId`)
    - Add route to delete a genre (`DELETE /:genreId`)
    - Integrate genre routes using `app.use('/genre', genreRoutes)`

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
+const express = require ('express')
+const movieRoutes = require('./routes/movieRoutes')
+const genreRoutes = require('./routes/genreRoutes')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/movies', movieRoutes); // All routes in `router` are prefixed with `/movies`
+app.use('/genre', genreRoutes); // All routes in `router` are prefixed with `/genre`
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
