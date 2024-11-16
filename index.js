01169da67b9499c9c740c314a482770dea7f8f88
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-17 04:08:56 +0530

    Added CORS middleware to enable Cross-Origin Resource Sharing

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require ('express')
+const cors = require('cors')
+
 const movieRoutes = require('./routes/movieRoutes')
 const genreRoutes = require('./routes/genreRoutes')
+
 const app = express()
 const port = 3000
+
+
+// CORS (Cross-Origin Resource Sharing) middleware is used to allow or restrict resource sharing between different origins (domains). 
+// It helps the server handle requests from external domains securely.
+app.use(cors());
+
 
 // Using express.json() middleware to automatically parse incoming JSON payloads.
 // This allows the API to process the request body as a JavaScript object, making
